40521705d7ad438d63e5973a72613c97d3228cfb
Ajay Umakanth <ajay@heidi-app.de>
2023-02-23 10:45:23 +0100

    Add mapping table and update filenames

---
+++ CoreDBMigrations/Migrations/20230223091714_UserCityUserMappingTable.cs
@@ -0,0 +1,30 @@
+﻿using FluentMigrator;
+
+namespace DatabaseMigrations.Migrations
+{
+    [Migration(20230223091714)]
+    public class UserCityUserMapping : Migration
+    {
+        public override void Up()
+        {
+            string sql =
+               @"DROP TABLE IF EXISTS User_CityUser_Mapping;
+                CREATE TABLE User_CityUser_Mapping (
+	                id int NOT NULL PRIMARY KEY AUTO_INCREMENT, 
+	                userId int,
+	                cityId int,
+	                cityUserId int
+                );";
+
+            Execute.Sql(sql);
+        }
+
+        public override void Down()
+        {
+            string sql =
+               @"DROP TABLE IF EXISTS User_CityUser_Mapping;";
+
+            Execute.Sql(sql);
+        }
+    }
+}
---
+++ CoreDBMigrations/Migrations/20230223092919_UserListingMappingTable.cs
@@ -0,0 +1,30 @@
+﻿using FluentMigrator;
+
+namespace DatabaseMigrations.Migrations
+{
+    [Migration(20230223092919)]
+    public class UserListingMapping : Migration
+    {
+        public override void Up()
+        {
+            string sql =
+               @"DROP TABLE IF EXISTS User_Listing_Mapping;
+                CREATE TABLE User_Listing_Mapping (
+	                id int NOT NULL PRIMARY KEY AUTO_INCREMENT, 
+	                userId int,
+	                cityId int,
+	                listingId int
+                );";
+
+            Execute.Sql(sql);
+        }
+
+        public override void Down()
+        {
+            string sql =
+               @"DROP TABLE IF EXISTS User_Listing_Mapping;";
+
+            Execute.Sql(sql);
+        }
+    }
+}
