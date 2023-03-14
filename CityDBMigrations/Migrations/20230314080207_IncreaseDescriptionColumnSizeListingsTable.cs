b0d388c98c8147e706fbfe7ab560ac0ecb024230
Ajay Umakanth <ajay@heidi-app.de>
2023-03-14 15:38:59 +0530

    Increase the size of the description column in listings table

---
+++ CityDBMigrations/Migrations/20230314080207_IncreaseDescriptionColumnSizeListingsTable.cs
@@ -0,0 +1,25 @@
+﻿using FluentMigrator;
+using System.Data.Common;
+
+namespace DatabaseMigrations.Migrations
+{
+    [Migration(20230314080207)]
+    public class IncreaseDescriptionColumnSizeListingsTable : Migration
+    {
+        public override void Up()
+        {
+            string sql =
+               @"ALTER TABLE heidi_city_1.listings CHANGE COLUMN description description VARCHAR(10000);";
+
+            Execute.Sql(sql);
+        }
+
+        public override void Down()
+        {
+            string sql =
+               @"ALTER TABLE heidi_city_1.listings CHANGE COLUMN description description VARCHAR(255); ";
+
+            Execute.Sql(sql);
+        }
+    }
+}
