5eab5bdcda4bf26cdec8dd23c1b5aabbd492b62b
Ajay Umakanth <ajay@heidi-app.de>
2023-04-13 11:45:34 +0200

    Add categories, subcategories and status tables in core database

---
+++ CoreDBMigrations/Migrations/20230413072959_AddStatusTable.cs
@@ -0,0 +1,28 @@
+﻿using FluentMigrator;
+
+namespace DatabaseMigrations.Migrations
+{
+    [Migration(20230413072959)]
+    public class AddStatusTable : Migration
+    {
+        public override void Up()
+        {
+            string sql =
+               @"DROP TABLE IF EXISTS status;
+                CREATE TABLE status(
+	                id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	                name varchar(255)
+                );
+                insert into status values (1,""Active""), (2,""Inactive""), (3,""Pending"");";
+            Execute.Sql(sql);
+        }
+
+        public override void Down()
+        {
+            string sql =
+               @"DROP TABLE IF EXISTS status;";
+
+            Execute.Sql(sql);
+        }
+    }
+}
