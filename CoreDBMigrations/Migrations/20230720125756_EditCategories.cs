3af9d2d0a79c01db5e399c601652430083b4cef8
SoSa-HEIDI <sonu@heidi-app.de>
2023-07-21 11:17:13 +0200

    category changes updated

--- CoreDBMigrations/Migrations/20230720125756_EditCategories.cs
+++ CoreDBMigrations/Migrations/20230720125756_EditCategories.cs
@@ -8,11 +8,13 @@ namespace DatabaseMigrations.Migrations
         public override void Up()
         {
             string sql =
-               @"UPDATE SUBCATEGORY SET name = ""FlashNews"" where id=1;
-               DELETE FROM CATEGORY WHERE name = ""Road works / Traffic"";
+               @"INSERT INTO SUBCATEGORY(id, name, categoryId) values (9,""Road works / Traffic"", 1);
+               UPDATE SUBCATEGORY SET name = ""FlashNews"" where id=1;
+               DELETE FROM CATEGORIES WHERE name = ""Road works / Traffic"";
                UPDATE SUBCATEGORY SET NAME = ""Road works / Traffic"" WHERE name = ""Alerts"";
-               DELETE FROM CATEGORY WHERE NAME =""Defect report"";
+               DELETE FROM CATEGORIES WHERE NAME =""Defect report"";
                DELETE FROM SUBCATEGORY WHERE name = ""Topic of the day"";
+               INSERT INTO CATEGORIES (name, noOfSubcategories) values (""Eat or Drink"", 0);
                ";
 
             Execute.Sql(sql);
