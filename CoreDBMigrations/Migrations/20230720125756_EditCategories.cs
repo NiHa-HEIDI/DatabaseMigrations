4612cf6fa738b62b7bd7ecfd4a44153abd4b41fb
SoSa-HEIDI <sonu@heidi-app.de>
2023-07-21 12:10:21 +0200

    subcategory delete fixed

--- CoreDBMigrations/Migrations/20230720125756_EditCategories.cs
+++ CoreDBMigrations/Migrations/20230720125756_EditCategories.cs
@@ -10,8 +10,8 @@ namespace DatabaseMigrations.Migrations
             string sql =
                @"INSERT INTO SUBCATEGORY(id, name, categoryId) values (9,""Road works / Traffic"", 1);
                UPDATE SUBCATEGORY SET name = ""FlashNews"" where id=1;
-               DELETE FROM CATEGORIES WHERE name = ""Road works / Traffic"";
-               UPDATE SUBCATEGORY SET NAME = ""Road works / Traffic"" WHERE name = ""Alerts"";
+               DELETE FROM CATEGORIES WHERE id = 2;
+               DELETE FROM SUBCATEGORY WHERE id = 2;
                DELETE FROM CATEGORIES WHERE NAME =""Defect report"";
                DELETE FROM SUBCATEGORY WHERE name = ""Topic of the day"";
                INSERT INTO CATEGORIES (name, noOfSubcategories) values (""Eat or Drink"", 0);
