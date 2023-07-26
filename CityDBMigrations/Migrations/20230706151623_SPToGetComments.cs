ab95aedaff32bd000a6de12a5056252295161dc0
Moiz-HEIDI <130048960+Moiz-HEIDI@users.noreply.github.com>
2023-07-26 14:13:12 +0200

    made get comments dynamic

--- CityDBMigrations/Migrations/20230706151623_SPToGetComments.cs
+++ CityDBMigrations/Migrations/20230706151623_SPToGetComments.cs
@@ -19,9 +19,9 @@ namespace DatabaseMigrations.Migrations
                         END;
                     START TRANSACTION;
                         SELECT  id, forumId, postId, comments, createdAt, userId, count(*) - 1 as ChildrenCount FROM (
-                        SELECT * FROM heidi_city_1.forumcomments fc WHERE fc.postId = postId AND fc.forumId = forumId AND fc.parentId is NULL
+                        SELECT * FROM forumcomments fc WHERE fc.postId = postId AND fc.forumId = forumId AND fc.parentId is NULL
                         UNION ALL
-                        SELECT fc1.* FROM heidi_city_1.forumcomments fc1 
+                        SELECT fc1.* FROM forumcomments fc1 
                         INNER JOIN 
                         heidi_city_1.forumcomments fc2 ON fc1.id = fc2.parentId
                         WHERE fc1.postId = postId AND fc1.forumId = forumId) AS t WHERE postId = postId AND forumId = forumId AND parentId is NULL GROUP BY id, forumId, postId, comments, createdAt, userId  LIMIT pageNo ,pageSize; 
