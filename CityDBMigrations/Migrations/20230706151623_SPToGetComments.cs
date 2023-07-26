16a27beb08d43d2e965b07cbcec5c467ecb77d6b
Moiz-HEIDI <130048960+Moiz-HEIDI@users.noreply.github.com>
2023-07-26 16:36:32 +0200

    camleCased the file and added sorting to comments

--- CityDBMigrations/Migrations/20230706151623_SPToGetComments.cs
+++ CityDBMigrations/Migrations/20230706151623_SPToGetComments.cs
@@ -2,11 +2,14 @@ using FluentMigrator;
 
 namespace DatabaseMigrations.Migrations
 {
-    [Migration(20230706151623)]
+    [Migration(20230726163023)]
     public class SPToGetComments : Migration
     {
         public override void Up()
         {
+            /*
+                26 July 2023 -> chnaged ChildCount to childCount AND sorted desc -Moiz
+            */
             string sql =
                @"
                 DROP PROCEDURE IF EXISTS sp_GetComments;
@@ -18,13 +21,13 @@ namespace DatabaseMigrations.Migrations
                             RESIGNAL;
                         END;
                     START TRANSACTION;
-                        SELECT  id, forumId, postId, comments, createdAt, userId, count(*) - 1 as ChildrenCount FROM (
+                        SELECT  id, forumId, postId, comments, createdAt, userId, count(*) - 1 as childrenCount FROM (
                         SELECT * FROM forumcomments fc WHERE fc.postId = postId AND fc.forumId = forumId AND fc.parentId is NULL
                         UNION ALL
                         SELECT fc1.* FROM forumcomments fc1 
                         INNER JOIN 
                         forumcomments fc2 ON fc1.id = fc2.parentId
-                        WHERE fc1.postId = postId AND fc1.forumId = forumId) AS t WHERE postId = postId AND forumId = forumId AND parentId is NULL GROUP BY id, forumId, postId, comments, createdAt, userId  LIMIT pageNo ,pageSize; 
+                        WHERE fc1.postId = postId AND fc1.forumId = forumId) AS t WHERE postId = postId AND forumId = forumId AND parentId is NULL GROUP BY id, forumId, postId, comments, createdAt, userId  ORDER BY createdAt DESC  LIMIT pageNo ,pageSize; 
 
 	                COMMIT;
                 END;";
