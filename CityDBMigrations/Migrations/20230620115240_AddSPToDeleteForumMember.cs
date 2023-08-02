7700a9a9daf96388595adeaabde09f7cb4f31c3c
SoSa-HEIDI <sonu@heidi-app.de>
2023-08-02 13:31:35 +0200

    post reports to reported posts

--- CityDBMigrations/Migrations/20230620115240_AddSPToDeleteForumMember.cs
+++ CityDBMigrations/Migrations/20230620115240_AddSPToDeleteForumMember.cs
@@ -2,11 +2,16 @@ using FluentMigrator;
 
 namespace DatabaseMigrations.Migrations
 {
-    [Migration(20230620115240)]
+    [Migration(20230802102777)]
     public class AddSPToDeleteForumMember : Migration
     {
         public override void Up()
         {
+            /*
+                Changes made
+                --------------------------------------------------------------------------
+                2 August 2023 -> changed postReports to RepostedPosts - Sonu
+            */
             string sql =
                @"
                 DROP PROCEDURE IF EXISTS sp_DeleteForumMember;
@@ -19,7 +24,7 @@ namespace DatabaseMigrations.Migrations
                         END;
                     START TRANSACTION;
 		                DELETE FROM forumcomments fc where fc.forumId = forumId and fc.userId = userId;
-		                DELETE FROM postreports pr where pr.forumId = forumId and pr.userId = userId;
+		                DELETE FROM ReportedPostes rp where rp.forumId = forumId and rp.userId = userId;
 		                DELETE FROM forumposts fp where fp.forumId = forumId and fp.userId = userId;
 		                DELETE FROM forummembers fm where fm.forumId = forumId and fm.userId = userId;
 	                COMMIT;
