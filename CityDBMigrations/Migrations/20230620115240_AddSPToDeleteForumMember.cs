7ddd5e4f79fa5fe942e9e5ebc6a121fb211be16b
Moiz-HEIDI <130048960+Moiz-HEIDI@users.noreply.github.com>
2023-09-13 16:06:58 +0200

    Changed SP DeleteForumMemeber

--- CityDBMigrations/Migrations/20230620115240_AddSPToDeleteForumMember.cs
+++ CityDBMigrations/Migrations/20230620115240_AddSPToDeleteForumMember.cs
@@ -12,21 +12,22 @@ namespace DatabaseMigrations.Migrations
                 --------------------------------------------------------------------------
                 2 August 2023 -> changed postReports to RepostedPosts - Sonu
                 10 August 2023 ->Added logic to remove reports made on the user - Ajay
+                13 Sept 2023 -> Changed ReportedPosts to reportedposts - Moiz
             */
             string sql =
                @"
                 DROP PROCEDURE IF EXISTS sp_DeleteForumMember;
                 CREATE PROCEDURE sp_DeleteForumMember (IN forumId int, IN userId int) 
                 BEGIN
-                    DECLARE EXIT HANDLER FOR SQLEXCEPTION 
+                    DECLARE EXIT HANDLER FOR SQLEXCEPTION  
                         BEGIN
                             ROLLBACK;
                             RESIGNAL;
                         END;
                     START TRANSACTION;
 		                DELETE FROM forumcomments fc where fc.forumId = forumId and fc.userId = userId;
-		                DELETE FROM ReportedPosts rp where rp.forumId = forumId and rp.userId = userId;
-                        DELETE rp FROM ReportedPosts rp
+		                DELETE FROM reportedposts rp where rp.forumId = forumId and rp.userId = userId;
+                        DELETE rp FROM reportedposts rp
                         INNER JOIN forumposts fp ON rp.postId = fp.id
                         WHERE fp.userId = userId;
 		                DELETE FROM forumposts fp where fp.forumId = forumId and fp.userId = userId;
