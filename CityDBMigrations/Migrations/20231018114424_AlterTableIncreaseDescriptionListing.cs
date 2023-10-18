be0905b49ec05140eb310d23e833a17a5747b244
AjUm-HEIDI <ajay@heidi-app.de>
2023-10-18 13:59:42 +0200

    Update description size

---
+++ CityDBMigrations/Migrations/20231018114424_AlterTableIncreaseDescriptionListing.cs
@@ -0,0 +1,21 @@
+using FluentMigrator;
+
+namespace DatabaseMigrations.Migrations
+{
+    [Migration(20231018114424)]
+    public class MakeExternalIDUnique : Migration
+    {
+        public override void Up()
+        {
+            string sql =
+               @"ALTER TABLE listings modify Description TEXT;
+               ";
+
+            Execute.Sql(sql);
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
