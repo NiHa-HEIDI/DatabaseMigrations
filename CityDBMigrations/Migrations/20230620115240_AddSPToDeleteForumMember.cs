5139c8770fe62eb7357956bf9e07d6a6ceceacc0
Moiz-HEIDI <130048960+Moiz-HEIDI@users.noreply.github.com>
2023-10-25 16:38:59 +0200

    Delete Forum and Delete Member updated

--- CityDBMigrations/Migrations/20230620115240_AddSPToDeleteForumMember.cs
+++ CityDBMigrations/Migrations/20230620115240_AddSPToDeleteForumMember.cs
@@ -2,7 +2,7 @@ using FluentMigrator;
 
 namespace DatabaseMigrations.Migrations
 {
-    [Migration(20230810131648)]
+    [Migration(20231025163027)]
     public class AddSPToDeleteForumMember : Migration
     {
         public override void Up()
@@ -13,6 +13,7 @@ namespace DatabaseMigrations.Migrations
                 2 August 2023 -> changed postReports to RepostedPosts - Sonu
                 10 August 2023 ->Added logic to remove reports made on the user - Ajay
                 13 Sept 2023 -> Changed ReportedPosts to reportedposts - Moiz
+                16 Oct 2023 -> Bugfix deleting comments gets forignkey constraint - Moiz
             */
             string sql =
                @"
@@ -25,13 +26,19 @@ namespace DatabaseMigrations.Migrations
                             RESIGNAL;
                         END;
                     START TRANSACTION;
-		                DELETE FROM forumcomments fc where fc.forumId = forumId and fc.userId = userId;
-		                DELETE FROM reportedposts rp where rp.forumId = forumId and rp.userId = userId;
+                        SET SQL_SAFE_UPDATES = 0;
+		                DELETE FROM forumcomments fc where fc.forumId = forumId and fc.userId = userId and fc.parentId is not NULL;
+                        DELETE fc1 FROM forumcomments fc1 INNER JOIN forumcomments fc2 ON fc1.parentId = fc2.id WHERE fc2.userId = userId and fc2.forumId = forumId;
+                        DELETE FROM forumcomments fc where fc.forumId = forumId and fc.userId = userId;
+                        DELETE FROM reportedposts rp where rp.forumId = forumId and rp.userId = userId;
                         DELETE rp FROM reportedposts rp
                         INNER JOIN forumposts fp ON rp.postId = fp.id
                         WHERE fp.userId = userId;
+                        DELETE fc1 FROM forumcomments fc1 INNER JOIN forumposts fc2 ON fc1.postId = fc2.id WHERE fc2.userId = userId and fc2.forumId = forumId and fc1.parentId IS NOT NULL;
+                        DELETE fc1 FROM forumcomments fc1 INNER JOIN forumposts fc2 ON fc1.postId = fc2.id WHERE fc2.userId = userId and fc2.forumId = forumId;
 		                DELETE FROM forumposts fp where fp.forumId = forumId and fp.userId = userId;
 		                DELETE FROM forummembers fm where fm.forumId = forumId and fm.userId = userId;
+                        SET SQL_SAFE_UPDATES = 1;
 	                COMMIT;
                 END;";
 
