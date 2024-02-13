d885ce872e3f527de4287a8f259fed7bc05fbe7e
AjUm-HEIDI <ajay@heidi-app.de>
2024-02-13 16:05:24 +0100

    Update the code to fetch the cityid

--- CityDBMigrations/Migrations/20240209104829_UpdateCoreUserIdColumn.cs
+++ CityDBMigrations/Migrations/20240209104829_UpdateCoreUserIdColumn.cs
@@ -12,7 +12,7 @@ namespace DatabaseMigrations.Migrations
                 UPDATE users u
                 INNER JOIN heidi_core.user_cityuser_mapping um ON u.id = um.cityUserId
                 SET u.coreUserId = um.userId
-                WHERE um.cityId = CONVERT(SUBSTRING_INDEX(REVERSE(DATABASE()), '_', 1), UNSIGNED INTEGER);
+                WHERE um.cityId = CAST(SUBSTRING_INDEX(SUBSTRING_INDEX(DATABASE(), '_', -1), '_', 1) AS UNSIGNED INTEGER);
                 ";
 
             Execute.Sql(sql);
