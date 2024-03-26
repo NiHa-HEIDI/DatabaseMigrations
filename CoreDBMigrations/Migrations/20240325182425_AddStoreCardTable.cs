c41a40bee3219684cdeb2a91f1099f835da44061
Aneesh Jose <aneesh@heidi-app.com>
2024-03-26 12:28:07 +0530

    feat:card related tables

---
+++ CoreDBMigrations/Migrations/20240325182425_AddStoreCardTable.cs
@@ -0,0 +1,37 @@
+using FluentMigrator;
+
+namespace DatabaseMigrations.Migrations
+{
+    [Migration(20240325182425)]
+    public class AddCardTable: Migration
+    {
+        public override void Up()
+        {
+            string sql =
+               @"DROP TABLE IF EXISTS store_card;
+                CREATE TABLE store_card (
+	                id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+                    createdAt DATETIME,
+                    updatedAt DATETIME,
+                    deletedAt DATETIME,
+                    shopId int,
+                    status int,
+                    userId int,
+                    balance double,
+                    expiryMonth int,
+                    expiryYear int,
+                    FOREIGN KEY (shopId) REFERENCES shops(id) ON DELETE CASCADE,
+                    FOREIGN KEY (userId) REFERENCES users(id)
+                );";
+            Execute.Sql(sql);
+        }
+
+        public override void Down()
+        {
+            string sql =
+               @"DROP TABLE IF EXISTS store_card;";
+
+            Execute.Sql(sql);
+        }
+    }
+}
