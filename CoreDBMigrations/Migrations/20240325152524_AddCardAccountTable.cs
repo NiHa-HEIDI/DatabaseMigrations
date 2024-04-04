00e7400a0468f4f88aa63ef6c03da675af6eaf6b
Aneesh Jose <aneesh@heidi-app.com>
2024-04-04 10:23:55 +0530

    refactor: add card account table, link transactions

--- CoreDBMigrations/Migrations/20240325152524_AddCardAccountTable.cs
+++ CoreDBMigrations/Migrations/20240325152524_AddCardAccountTable.cs
@@ -2,25 +2,22 @@ using FluentMigrator;
 
 namespace DatabaseMigrations.Migrations
 {
-    [Migration(20240325182425)]
-    public class AddStoreCardTable: Migration
+    [Migration(20240325152524)]
+    public class AddCardAccountTable: Migration
     {
         public override void Up()
         {
             string sql =
-               @"DROP TABLE IF EXISTS store_card;
-                CREATE TABLE store_card (
+               @"DROP TABLE IF EXISTS card_account;
+                CREATE TABLE card_account (
 	                id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
                     createdAt DATETIME,
                     updatedAt DATETIME,
                     deletedAt DATETIME,
                     status int,
                     userId int,
-                    accountId int,
-                    expiryMonth int,
-                    expiryYear int,
-                    FOREIGN KEY (userId) REFERENCES users(id),
-                    FOREIGN KEY (accountId) REFERENCES card_account(id)
+                    balance double,
+                    FOREIGN KEY (userId) REFERENCES users(id)
                 );";
             Execute.Sql(sql);
         }
@@ -28,7 +25,7 @@ namespace DatabaseMigrations.Migrations
         public override void Down()
         {
             string sql =
-               @"DROP TABLE IF EXISTS store_card;";
+               @"DROP TABLE IF EXISTS card_account;";
 
             Execute.Sql(sql);
         }
