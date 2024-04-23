4f1f22bc93a02ef73150a2e3ba0d95e60448866b
Moiz-HEIDI <130048960+Moiz-HEIDI@users.noreply.github.com>
2024-04-23 15:28:58 +0200

    metaData -> metadata in appointments table

---
+++ CoreDBMigrations/Migrations/20242304151342_ChnageColumnNameAppointmentTale.cs
@@ -0,0 +1,24 @@
+using FluentMigrator;
+
+namespace DatabaseMigrations.Migrations
+{
+    [Migration(20242304151342)]
+    public class ChangeColumnNameMetaData : Migration
+    {
+        public override void Up()
+        {
+            string sql =
+                @"ALTER TABLE appointments RENAME COLUMN metaData TO metadata;";
+
+            Execute.Sql(sql);
+        }
+
+        public override void Down()
+        {
+            string sql =
+               @"ALTER TABLE appointments RENAME COLUMN metadata TO metaData;";
+
+            Execute.Sql(sql);
+        }
+    }
+}
