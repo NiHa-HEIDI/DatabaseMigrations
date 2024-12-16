53698c1c5e0bab0bd424c6cd9861c201d1acc805
Aneesh Jose <aneesh@heidi-app.com>
2024-12-16 15:02:49 +0530

    added migration to insert Handel and Geschäfte categories with subcategories in the database

---
+++ CoreDBMigrations/Migrations/20241216092556_InsertHandelAndGeschafteCategories.cs
@@ -0,0 +1,35 @@
+using FluentMigrator;
+
+namespace CoreDBMigrations.Migrations
+{
+    [Migration(20241216092556)]
+    public class InsertHandelAndGeschafteCategories : Migration
+    {
+        public override void Up()
+        {
+            string sql =
+               @"INSERT INTO categories (id, name, isEnabled, category_order)
+                VALUES 
+                    (45, 'Handel', false, 45),
+                    (46, 'Geschäfte', false, 46),
+                    (47, 'Dienstleister', false, 47)
+                ON DUPLICATE KEY UPDATE
+                    name = name,
+                    isEnabled = true,
+                    category_order = category_order;
+                INSERT INTO subcategory (id, name, categoryId)
+                VALUES
+                    (12, 'Handwerk', 47),
+                    (13, 'Gesundheit', 47),
+                    (14, 'Immobilien', 47),
+                    (15, 'Finanzen', 47);
+                ";
+
+            Execute.Sql(sql);
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
