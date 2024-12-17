3facaca5db3ba5a1a420014da1610a573f9b01b1
Aneesh Jose <aneesh@heidi-app.com>
2024-12-17 14:16:25 +0530

    migration: insert Dienstleister category and associated subcategories in city database

---
+++ CityDBMigrations/Migrations/20241216094108_InsertDienstleisterCategory.cs
@@ -0,0 +1,37 @@
+using FluentMigrator;
+
+namespace DatabaseMigrations.Migrations
+{
+    [Migration(20241216094108)]
+    public class InsertDienstleisterCategory : Migration
+    {
+        public override void Up()
+        {
+            string sql =
+               @"INSERT INTO categories (id, name)
+                VALUES 
+                    (45, 'Dienstleister');
+                INSERT INTO subcategory (id, name, categoryId)
+                VALUES
+                    (12, 'Handwerk', 45),
+                    (13, 'Gesundheit', 45),
+                    (14, 'Immobilien', 45),
+                    (15, 'Finanzen', 45);
+                ";
+
+            Execute.Sql(sql);
+        }
+
+        public override void Down()
+        {
+            string sql =
+               @"
+                DELETE FROM subcategory WHERE id IN (12, 13, 14, 15);
+                DELETE FROM categories WHERE id IN (45);
+                ";
+            Execute.Sql(sql);
+
+
+        }
+    }
+}
