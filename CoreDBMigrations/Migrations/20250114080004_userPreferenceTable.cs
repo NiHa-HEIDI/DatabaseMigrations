379daf179ec7fa1d224de589ed8f13ab7f24187d
SoSa-HEIDI <sonu@heidi-app.de>
2025-01-16 10:52:23 +0100

    user preference for category and cities

--- CoreDBMigrations/Migrations/20250114080004_userPreferenceTable.cs
+++ CoreDBMigrations/Migrations/20250114080004_userPreferenceTable.cs
@@ -3,25 +3,31 @@ using FluentMigrator;
 namespace CoreDBMigrations.Migrations
 {
     [Migration(20250114080004)]
-    public class AddUserNotificationPreference : Migration
+    public class AddUserPreference : Migration
     {
         public override void Up()
         {
             string sql =
                @"CREATE TABLE user_preferences (
                     id INT AUTO_INCREMENT PRIMARY KEY,
-                    userId INT NOT NULL,                          
-                    categoryIds VARCHAR(255),                      
+                    userId INT NOT NULL,                      
                     createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
                     updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, 
                     FOREIGN KEY (userId) REFERENCES users(id)
                 );
                 CREATE TABLE user_preference_cities (
-                    preferenceCityId INT AUTO_INCREMENT PRIMARY KEY, 
+                    id INT AUTO_INCREMENT PRIMARY KEY, 
                     preferenceId INT NOT NULL,                       
                     cityId INT NOT NULL,                             
                     FOREIGN KEY (preferenceId) REFERENCES user_preferences(id), 
                     FOREIGN KEY (cityId) REFERENCES cities(id)
+                );
+                CREATE TABLE user_preference_categories (
+                    id INT AUTO_INCREMENT PRIMARY KEY, 
+                    preferenceId INT NOT NULL,                       
+                    categoryId INT NOT NULL,                             
+                    FOREIGN KEY (preferenceId) REFERENCES user_preferences(id), 
+                    FOREIGN KEY (categoryId) REFERENCES categories(id)
                 );";
 
             Execute.Sql(sql);
@@ -31,6 +37,7 @@ namespace CoreDBMigrations.Migrations
         {
             string sql =
                @"DROP TABLE IF EXISTS user_preference_cities;
+                DROP TABLE IF EXISTS user_preference_categories;
                 DROP TABLE IF EXISTS user_preferences;";
 
             Execute.Sql(sql);
